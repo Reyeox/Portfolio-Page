9dfea8784ba490d716f1e13798c9ee92f86a53f0
Eduardo Hernández <reyeox@gmail.com>
2018-02-07 14:28:52 -0400

    Estilos y R

--- src/App.js
+++ src/App.js
@@ -5,15 +5,32 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <nav class="navbar navbar-default" role="navigation">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
+            <span class="sr-only">Toggle navigation</span>
+          </button>
+          <a class="navbar-brand" href="#">Portfolio Reyeox</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-collapse-01">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#fakelink">Projects</a></li>
+            <li><a href="#fakelink">Certified</a></li>
+            <li><a href="#fakelink">Contact</a></li>
+            <li><a href="#fakelink">Upload Project</a></li>
+           </ul>
+          <form class="navbar-form navbar-right" action="#" role="search">
+            <div class="form-group">
+              <div class="input-group">
+                <input class="form-control" id="navbarInput-01" type="search" placeholder="Search"/>
+                <span class="input-group-btn">
+                  <button type="submit" class="btn"><span class="fui-search"></span></button>
+                </span>
+              </div>
+            </div>
+          </form>
+        </div>
+      </nav>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './dist/css/flat-ui.css';
+import './dist/css/flat-ui.css.map';
+import './dist/css/flat-ui.min.css';
+import './dist/css/vendor/bootstrap/css/bootstrap.min.css';
+
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
